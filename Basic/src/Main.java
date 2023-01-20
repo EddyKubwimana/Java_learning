39e10b68e73cf9f94793160fa8e8d757274244dc
Eddy Kubwimana <92218436+EddyKubwimana@users.noreply.github.com>
2023-01-20 00:00:40 +0000

    Add files via upload

---
+++ Basic/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+     public static void main(String[] args) {
+        int number1 = 20;
+        int number2 = 30;
+        char n = 'A';
+        System.out.println("Hello my dear friends");
+        System.out.println(number1+number2);
+        System.out.println(n+ " "+ "java sucks!");
+
+        
+    }
+    
+}
