b2aee773e6e4c8df4b1cebde419057ab45a694b8
Eddy Kubwimana <92218436+EddyKubwimana@users.noreply.github.com>
2023-02-10 23:10:23 +0000

    Add files via upload

--- Basic/assignment.java
+++ Basic/assignment.java
@@ -4,14 +4,17 @@ public static void main(String[] args){
     double suzukiPrice = 12000;
     double harleyPrice = 2000;
     Person_ girl = new Person_("Anisa", 21, "female");
-    System.out.println("Girl: "+ girl.getName());
+    
     Person_ woman = new Person_("Mansah", 39, "female");
     Person_ guy = new Person_("kofi",25, "male");
-    
-    Motorbike bike = new Motorbike(girl, suzukiPrice);
+
+
+    Person_ eddy = girl;
+
+    Motorbike bike = new Motorbike(eddy, suzukiPrice);
     Person_ theBoss = bike.getOwner();
     
-    //theBoss.setName("zoey");
+    theBoss.setName("zoey");
     System.out.println("Bike Owner  :"+ bike.getOwnersName() );
     System.out.println("Girl: "+ girl.getName());
 
