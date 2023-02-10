3914c9091150808c8daee63239582c18bdb2bc21
Eddy Kubwimana <92218436+EddyKubwimana@users.noreply.github.com>
2023-02-10 22:18:17 +0000

    Add files via upload

---
+++ Basic/Person_.java
@@ -0,0 +1,24 @@
+public class Person_{
+    private String name;
+    private int age;
+    private String gender;
+
+    public Person_(String name, int age, String gender){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+
+    }
+    public String getName(){
+        return this.name;
+    }
+    public int getAge(){
+        return this.age;
+    }
+    public String getGender(){
+        return this.gender;
+    public Person_(){
+        return Person_;
+    
+    }
+}
