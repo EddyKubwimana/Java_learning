371ad7f2c1b8dcfc8f460ad25dfe6724d77e1280
Eddy Kubwimana <92218436+EddyKubwimana@users.noreply.github.com>
2023-02-23 22:32:20 +0000

    Add files via upload

--- Basic/testSharedMethod.java
+++ Basic/testSharedMethod.java
@@ -5,10 +5,10 @@ public class testSharedMethod{
         sharedMethod ora1 = new sharedMethod("embe", 20);
         
         sharedMethod ora2 = new sharedMethod("save", 20);
-    
-
-        System.out.println(ora1.getKind());
         System.out.println(ora2.getKind());
+        sharedMethod.setKind("Mangoes");
+        System.out.println(ora2.getKind());
+        System.out.println(sharedMethod.getKind());
 
 
     }
